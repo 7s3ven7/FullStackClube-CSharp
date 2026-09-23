15d339bae2abe7104f09a40d24e0e657228e3db3
Wesley Gabriel Trentin Rodrigues <7s3ven7.1@gmail.com>
2026-09-23 18:43:36 -0300

    Classes e Objetos

---
+++ POO/POO/Mouse.cs
@@ -0,0 +1,33 @@
+﻿namespace POO
+{
+    public class Mouse
+    {
+
+        public string Cor { get; set; }
+        public int Botoes { get; set; }
+        public string Tipo { get; set; }
+        
+        public Mouse(string cor, int botoes, string tipo)
+        {
+            Cor = cor;
+            Botoes = botoes;
+            Tipo = tipo;
+        }
+
+        public void Clicar()
+        {
+            Console.WriteLine("Clicando...");
+        }
+
+        public void Rolar()
+        {
+            Console.WriteLine("Rolando a página...");
+        }
+
+        public void CorDoMouse()
+        {
+            Console.WriteLine($"Cor do Mouse: {Cor}");
+        }
+
+    }
+}
---
+++ POO/POO/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mouse mouseBranco = new Mouse("Branco", 2, "Sem Fio");
+
+            Console.WriteLine($"Cor do mouse: {mouseBranco.Cor}");
+            Console.WriteLine($"Quantidade de Botões do mouse: {mouseBranco.Botoes}");
+            Console.WriteLine($"tipo do mouse: {mouseBranco.Tipo}");
+
+            mouseBranco.Clicar();
+            mouseBranco.Rolar();
+
+            Mouse mousePreto = new Mouse("Preto", 6, "Com Fio");
+
+            mousePreto.CorDoMouse();
+        }
+    }
+}
